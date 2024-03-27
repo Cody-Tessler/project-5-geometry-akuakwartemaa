d19da74dabe4aa09948c35847b799c27d72819d3
akuakwartemaa <150639539+akuakwartemaa@users.noreply.github.com>
2024-03-27 04:55:27 +0000

    pentagon progress

--- pentagon.py
+++ pentagon.py
@@ -1,23 +1,26 @@
 from shape import Shape
+from math import sqrt
 
 class Pentagon(Shape):
 
     def __init__(self, a):
+        self.a=a
         ...
 
     def get_area(self):
-        return 0
+        return (1/4) * sqrt(5 * (5 + 2 * sqrt(5))) * self.a ** 2
 
     def get_perimeter(self):
-        return 0
+        return 5 * self.a
 
     def __str__(self):
-        return "TODO"
+        return (f"Pentagon with side {self.a:.2f}, Area: {self.get_area():.2f}, "
+                f"Perimeter: {self.get_perimeter():.2f}")
 
     @classmethod
     def get_area_formula(cls):
-        return "TODO"
+        return "1/4 * sqrt(5 * (5 + 2 * sqrt(5))) * a^2"
 
     @classmethod
     def get_perimeter_formula(cls):
-        return "TODO"
+        return "5 * a"
